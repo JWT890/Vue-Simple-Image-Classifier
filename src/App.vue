7e5c5c68e2cf9f1ff91aaf3bb6a5326418e7cd78
Jon Taylor <95875505+JWT890@users.noreply.github.com>
2023-11-01 15:37:47 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Image Classifier App</h1>
+    </header>
+    <main>
+      <SimpleImageClassifier />
+    </main>
+  </div>
+</template>
+
+<script>
+import SimpleImageClassifier from './components/SimpleImageClassifier.vue';
+
+export default {
+  components: {
+    SimpleImageClassifier,
+  },
+};
+</script>
